30efc42e2ed87fc00409248955996bc87e42cbd8
Tiger-Foxx <donfackarthur750@gmail.com>
2024-09-16 23:05:40 +0100

    preparation de la vue index

--- Fox/urls.py
+++ Fox/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from FoxApp.views import *
+from Fox import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', index, name='index')
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
