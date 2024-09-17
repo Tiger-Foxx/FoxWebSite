238ab85cacc023ec106864dc4c2e8a3f8c5b68cd
Tiger-Foxx <donfackarthur750@gmail.com>
2024-09-17 09:23:36 +0100

    ajout des modeles de projet et de prodile

--- FoxApp/models.py
+++ FoxApp/models.py
@@ -9,6 +9,8 @@ class Profile(models.Model):
     descriptionP1=models.TextField()
     descriptionP2=models.TextField()
     signature=models.ImageField(upload_to='PhotoFox')
+    email=models.EmailField()
+    telephone=models.CharField(max_length=328)
     cv=models.FileField(upload_to='FilesFox',blank=True,null=True)
 
     def __str__(self):
@@ -29,4 +31,41 @@ class Project(models.Model):
     cv=models.FileField(upload_to='FilesFox')
 
     def __str__(self):
-        return f"MON PROFILE | {self.nom.capitalize()}" 
+        return f"PROJET | {self.nom.capitalize()} | {self.categorie.capitalize()}" 
+    
+
+class Post(models.Model):
+    titre=models.CharField(max_length=328)
+    description=models.TextField()
+    photo500_x_800=models.ImageField(upload_to='PhotoFox',blank=True)
+    photo800_x_533=models.ImageField(upload_to='PhotoFox',blank=True)
+    categorie=models.CharField(max_length=328)
+    auteur=models.CharField(max_length=328,blank=True)
+    date=models.DateField(auto_now_add=True)
+    contenuP1=models.TextField(blank=True,null=True)
+    contenuConclusion=models.TextField(blank=True,null=True)
+    contenuSitation=models.TextField(blank=True,null=True)
+
+
+    def __str__(self):
+        return f"ARTICLE | {self.titre.capitalize()} | {self.date}" 
+    
+
+class Visiteur(models.Model):
+    email=models.EmailField()
+    nom=models.CharField(max_length=100,blank=True,null=True)
+    def __str__(self):
+        return f"VISITEUR | {self.email}"
+
+class Commentaire(models.Model):
+    contenu=models.TextField()
+    visiteur=models.ForeignKey(Visiteur,on_delete=models.CASCADE)
+    post=models.ForeignKey(Post, on_delete=models.CASCADE)
+    date=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"COMMENTAIRE SUR | {self.post.titre} le {self.date}"
+
+    
+
+    
